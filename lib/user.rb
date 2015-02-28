c552dd925616afaa555383974b6bf68be4d6d8ed
James Ottaway <git@james.ottaway.io>
2015-02-28 10:09:45 +1000

    Implement `#to_model` and `#to_repository`

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require 'active_model'
+require 'user/repository'
 
 class User
   include ActiveModel::Model
@@ -7,4 +8,14 @@ class User
   attr_accessor :name
 
   validates :name, presence: true
+
+  def attributes
+    { id: self.id, name: self.name }.stringify_keys
+  end
+
+  def to_repository
+    User::Repository.where(id: self.id).first_or_initialize.tap do |repository|
+      repository.assign_attributes(self.attributes)
+    end
+  end
 end
--- lib/user/repository.rb
+++ lib/user/repository.rb
@@ -1,8 +1,15 @@
 require 'active_record'
+require 'user'
 
 class User
   class Repository < ActiveRecord::Base
     self.table_name = 'users'
     validates :name, uniqueness: true
+
+    def to_model
+      User.new(self.attributes.except('id')).tap do |user|
+        user.instance_variable_set :@id, self.id
+      end
+    end
   end
 end
