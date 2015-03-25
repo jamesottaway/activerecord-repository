28a65db1a78ad2a2b823537e6bfb31f8d6531999
James Ottaway <git@james.ottaway.io>
2015-03-26 08:21:45 +1000

    Wrap all RSpec examples in a transaction and roll it back

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,12 @@ require 'rspec/its'
 require 'rspec/the'
 require 'shoulda-matchers'
 require 'pry'
+
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    ActiveRecord::Base.connection.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
